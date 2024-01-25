75f898b2c705710f1f4ef807d6f2c7219b6b7f80
Annika Shankwitz <annikashankwitz@Interstellar.local>
2024-01-25 17:43:58 -0500

    :)

--- encoding.py
+++ encoding.py
@@ -1,6 +1,8 @@
 #read in lines
 import sys
 lines = sys.stdin.readlines()
+import unicodedata
+
 
 #get regex
 import re
@@ -13,19 +15,31 @@ invalid = defaultdict(lambda:0)
 valid = ""
 
 for line in lines:
+	
+	#normalized the spaces
+	cleanLine = ""
+	for character in line:
+		if unicodedata.category(character) == 'Fs':
+			invalid[ord(character)] += 1
+		elif unicodedata.category(character) == 'Zs' and character != " " and character != "\n":
+			invalid[ord(character)] += 1
+			cleanLine += " " 
+		else:
+			cleanLine += character
 
-	if re.search("[^a-zA-ZäÄöÖüÜß!:;.?\"\'\s]", line):
-		for character in line:
-			if re.match("[^a-zA-ZäÄöÖüÜß!.?\"\']", character):
+	#replaces invalid characters that are not spaces
+	if re.search("[^a-zA-ZäÄöÖüÜß!;.?\"\'\s]", cleanLine):
+		for character in cleanLine:
+			if re.match("[^a-zA-ZäÄöÖüÜß!;.?\"\'\s]", character):
 				invalid[character] += 1
+		
 			
 	else:
 		if not line.isspace() :
-			valid += line
-			print(line)	
+			valid += cleanLine	
 	 
  
 sys.stdout.write(valid)
 
-for x  in invalid.keys():
-	sys.stderr.write(x + "\t" + str(invalid[x]) + "\n")
+for x in invalid.keys():
+	sys.stderr.write(str(x) + "\t" + str(invalid[x]) + "\n")
